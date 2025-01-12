d777bb691dec03cba0a5861c4f83cb6be60f51e3
Sotoodeh <sotoodeh.n22@gmail.com>
2025-01-12 20:17:03 +0100

    Develop Custom User Authentication and Authorization for login and logout.

---
+++ src/main/java/Sotoodeh/jobportal/config/WebSequrityConfig.java
@@ -0,0 +1,64 @@
+package Sotoodeh.jobportal.config;
+
+import Sotoodeh.jobportal.services.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+
+@Configuration
+public class WebSequrityConfig {
+
+    @Autowired
+    private final CustomUserDetailsService customUserDetailsService;
+
+    private final String[]publicUrl = {"/",
+            "/global-search/**",
+            "/register",
+            "/register/**",
+            "/webjars/**",
+            "/resources/**",
+            "/assets/**",
+            "/css/**",
+            "/summernote/**",
+            "/js/**",
+            "/*.css",
+            "/*.js",
+            "/*.js.map",
+            "/fonts**", "/favicon.ico", "/resources/**", "/error"};
+    @Autowired
+    public WebSequrityConfig(CustomUserDetailsService customUserDetailsService) {
+        this.customUserDetailsService = customUserDetailsService;
+    }
+
+    @Bean
+    protected DefaultSecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authenticationProvider(authenticationProvider());
+        http.authorizeHttpRequests(auth ->{
+            auth.requestMatchers(publicUrl).permitAll();
+            auth.anyRequest().authenticated();
+        });
+        return http.build();
+    }
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        authenticationProvider.setUserDetailsService(customUserDetailsService);
+        return  authenticationProvider;
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
